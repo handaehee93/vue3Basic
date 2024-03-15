a10bc924c6cec8454b83b529e3f681fca00601ac
HanDaeHee <hdh89900@gmail.com>
2024-03-15 09:47:41 +0900

    반응형 API인 ref를 사용하여 반응형 데이터 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>반응형 메세지</h1>
+    {{ reactiveMessage }}
+    <button v-on:click="addReactiveMessage">반응형 메시지 변경 하기</button>
+    <br>
+    <h1>일반 메세지</h1>
+    {{ normalMessage }}
+    <button v-on:click="addNormalMessage">일반 메시지 변경 하기</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import {ref}  from 'vue';
+  export default {
+    setup(){
+      // ref와 같은 반응형 API를 사용하면 데이터를 반응형으로 만들 수 있다.
+      const reactiveMessage = ref("reactive 메시지")
+      const addReactiveMessage = () => {
+        reactiveMessage.value = reactiveMessage.value + '!'
+      }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+      // 반응형 데이터가 아니면 버튼을 클릭했을때 실시간으로 데이터가 변경이 되지 않는 것을 볼 수 있다.
+      let normalMessage = "noraml 메세지"
+      const addNormalMessage = () => {
+        normalMessage = normalMessage + '!'
+      }
+      return {
+        reactiveMessage,
+        addReactiveMessage,
+        normalMessage,
+        addNormalMessage
+      }
+    }
   }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<style lang="scss" scoped>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
