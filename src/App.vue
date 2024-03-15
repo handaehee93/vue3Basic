40045f7c15e81e306d36ce4c129a5f202cb69c03
HanDaeHee <hdh89900@gmail.com>
2024-03-15 10:35:35 +0900

    라이프사이클 훅 예시

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import {ref}  from 'vue';
+import {ref, onMounted, onBeforeMount}  from 'vue';
   export default {
     setup(){
       // ref와 같은 반응형 API를 사용하면 데이터를 반응형으로 만들 수 있다.
@@ -25,6 +25,19 @@ import {ref}  from 'vue';
       const addNormalMessage = () => {
         normalMessage = normalMessage + '!'
       }
+
+      // 뷰 인스턴스가 생성되고 종료되는 과정 에서 사용되는 '라이프사이클 훅'이 있다.
+      // 예를 들면 아래와 같이 onBeforeMount함수를 사용해 마운트 되기 전에 호출되는 로직을 지정하고
+      // onMount를 사용해 마운트 되면 호출되는 로직를 지정할 수 있는 것. 
+      onBeforeMount(() => {
+        console.log("비포 마운트")
+      })
+
+      onMounted(() => {
+        console.log("마운트 되었음")
+      })
+
+
       return {
         reactiveMessage,
         addReactiveMessage,
