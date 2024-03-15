327a1b98426bec3561f734d9be8d626a0dbd1f74
HanDaeHee <hdh89900@gmail.com>
2024-03-15 11:14:44 +0900

    템플릿 문법

---
+++ src/lectures/1/CompositionApi.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>반응형 메세지</h1>
+    {{ reactiveMessage }}
+    <button v-on:click="addReactiveMessage">반응형 메시지 변경 하기</button>
+    <br>
+    <h1>일반 메세지</h1>
+    {{ normalMessage }}
+    <button v-on:click="addNormalMessage">일반 메시지 변경 하기</button>
+  </div>
+</template>
+
+<script>
+import {ref, onMounted, onBeforeMount}  from 'vue';
+  export default {
+    setup(){
+      // ref와 같은 반응형 API를 사용하면 데이터를 반응형으로 만들 수 있다.
+      const reactiveMessage = ref("reactive 메시지")
+      const addReactiveMessage = () => {
+        reactiveMessage.value = reactiveMessage.value + '!'
+      }
+
+      // 반응형 데이터가 아니면 버튼을 클릭했을때 실시간으로 데이터가 변경이 되지 않는 것을 볼 수 있다.
+      let normalMessage = "noraml 메세지"
+      const addNormalMessage = () => {
+        normalMessage = normalMessage + '!'
+      }
+
+      // 뷰 인스턴스가 생성되고 종료되는 과정 에서 사용되는 '라이프사이클 훅'이 있다.
+      // 예를 들면 아래와 같이 onBeforeMount함수를 사용해 마운트 되기 전에 호출되는 로직을 지정하고
+      // onMount를 사용해 마운트 되면 호출되는 로직를 지정할 수 있는 것. 
+      onBeforeMount(() => {
+        console.log("비포 마운트")
+      })
+
+      onMounted(() => {
+        console.log("마운트 되었음")
+      })
+
+
+      return {
+        reactiveMessage,
+        addReactiveMessage,
+        normalMessage,
+        addNormalMessage
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/lectures/2/TemplateSyntax.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- {{  }}를 통해 반응형 데이터를 바인딩 할 수 있다. -->
+    <h3> {{ message }} </h3>
+    
+    <!-- {{  }}에서는 데이터를 표시해줄 뿐만 아니라 자바스크립트 표현식도 사용할 수 있다. -->
+    <h3>{{ message.split('').reverse() }}</h3>
+    <h3>{{ message.length > 10 ? "10보다 크다" : "10보다 작다" }}</h3>
+    
+    <!-- v-once를 사용하면 처음 값만 가져오고 변경이 되지 않게 할 수 있다. -->
+    <h3 v-once>{{ message }}</h3>
+    
+    <!-- 버튼 클릭시 v-once가 붙어 있는 message는 변경이 되지 않는다. -->
+    <button v-on:click="message = message + '!'">메세지 변경</button>
+
+------------------------------------------------------------------------------------------------------------------------------
+    <!-- 속성에 바인딩을 할 때는 v-bind를 사용한다.  -->
+    <!-- title은 마우스를 hover했을 때 나타날 텍스트를 지정하는 속성인데 해당 속성에 binding을 걸어 반응형 데이터를 넣을 수 있다. -->
+    <div title="안녕하세요">마우스를 올려서 속성 확인</div>
+    <div v-bind:title="bindingTitle">마우스를 올려서 속성 확인</div>
+    
+    <!-- disabled 속성은 input창의 입력을 막는 속성인데 이렇게 속성 존재 자체가 참,거짓을 의미하는 boolean 속성도 바인딩 할 수 있다. -->
+    <input type="text" disabled>
+    <input type="text" v-bind:disabled="isInputDisabled"/>
+    
+    <!-- v-bind는 :로 단축하여 사용할 수 있다. -->
+    <input type="text" :disabled="isInputDisabled"/>
+    
+    <!-- 여러 속성을 한번에 바인딩 할 수 도 있다. -->
+    <input v-bind:="attrs">
+  </div>  
+</template>
+
+<script>
+import {ref} from 'vue'
+
+  export default {
+    setup(){
+      const message =  ref("안녕하세요")
+      const bindingTitle = ref("title속성에 바인딩하기")
+      const isInputDisabled = ref(false)
+      const attrs = ref({
+        type:'password',
+        value: 123,
+        disabled: true
+      })
+
+      return {
+        message,
+        bindingTitle,
+        isInputDisabled,
+        attrs
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
