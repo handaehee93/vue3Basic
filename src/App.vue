cd1c002fdfdcc024c49805173ceed9397bf77dfd
handaehee93 <hdh8990@naver.com>
2024-03-17 13:48:47 +0900

    v-directive

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,13 @@
 <template>
-  <!-- 반복하고 싶은 요소에 v-for를 통해 요소를 반복 출력한다. 이때 고유한 값인 key값을 반드시 넣어줘야 한다. -->
-  <!-- 배열의 각 요소와 index를 가져올 수 있다. -->
-  <div v-for="(item, index) in items" :key="item.id">
-    아이템 이름 : {{ item.message }}
-    인덱스 : {{ index }}
-  </div>
-  <hr>
-  <!-- v-for와 v-if를 동시에 사용할 순 없다. 반복문 내에서 조건문을 사용하고 싶다면 아래와 같이 template태그를 사용해야 한다. -->
-  <template v-for="item in items" :key="item.id">
-    <div v-if="item.id % 2 === 0">{{ item.message }}</div>
-  </template>
+  <div>
 
-  ------------------------------------------------------------------------------------------------------------------------------
-  <!-- 배열 뿐 아니라 객체도 반복해서 출력할 수 있다. 객체의 경우 value(값), key(키 값), index(순서) 순으로 값을 받아온다. -->
-  <div v-for="(value, key, index) in obj" :key="key">
-    키 : {{ key }} / 벨류 : {{ value }} / 인덱스 : {{ index }}
   </div>
-
 </template>
 
 <script>
-import {reactive} from 'vue'
-
-export default {
-  setup () {
-    const items = reactive([
-    {id: 1, message: 'javaScript'},
-    {id: 2, message: 'java'},
-    {id: 3, message: 'python'},
-  ])
-
-    const obj = reactive({
-      title:"제목",
-      author: "hdh",
-      date : "2024-03-15"
-    })
-
-  return {items,obj}
+  export default {
+    
   }
-}
-
 </script>
 
 <style lang="scss" scoped>
---
+++ src/lectures/VueDirective.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- 디렉티브는 'v-' 접두사가 있는 특수 속성이다. -->
+    <!-- 디렉티브는 말 그대로 컴포넌트 또는 DOM요소에게 "~하게 동작해라" 라고 지시하는 지시문이다. -->
+
+    <!-- "v-text"는 요소의 텍스트 컨텐츠를 업데이트 하게 하는 지시문이다. -->
+    <div v-text="msg"></div>
+
+    <!-- "v-html"은 요소의 컨텐츠를 html로 인식하게 한다.-->
+    <div v-html="htmlStr"></div>
+    <div v-text="htmlStr"></div>
+
+    <!-- "v-clock"은 추가학습 필요 -->
+
+    <!-- "v-once는" 컴포넌트를 한번만 렌더링 하게 한다. 한번 렌더링 된 후 반응형 데이터가 변경이 되도 변경이 되지 않는다. -->
+    <div v-once>{{ msg }}</div>
+    <button v-on:click="msg = msg + '!'"> msg 반응형 데이터 변경</button>
+
+    <!-- "v-memo"는 "v-once"의 업그레이드 버전이다. "v-memo"에 배열을 넣고 그 안에 반응형 데이터를 넣어 두면 해당 데이터가 변경이 되었을 때만 요소의 변경이 일어난다. 아래 예시에서 memmoNUmber2는 배열에 없기 때문에 버튼을 클릭해도 숫자가 증가하지 않는다.-->
+    <div v-memo="[memoNumber]"> memoNumber : {{ memoNumber }} , memoNumber2 : {{ memoNumber2 }}</div>
+    <button @click="memoNumber++">memoNumber 숫자 증가</button>
+    <button @click="memoNumber2++">memoNumber2 숫자 증가</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+  export default {
+    setup () {
+      const msg = ref("안녕하세요")
+      const htmlStr = ref("<div>html 요소 추가</div>")
+      let memoNumber = ref(1)
+      let memoNumber2 = ref(1)
+
+      return {
+        msg,
+        htmlStr,
+        memoNumber,
+        memoNumber2,
+        increaseNum
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
